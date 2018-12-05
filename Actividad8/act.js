2d34cd0882e8af660393161282b81f8cf2040bca
LAPTOP-6J5DAT7D\Carlita <cacaru@floridauniversitaria.es>
2018-12-05 12:37:01 +0100

    actividad8 2.0

--- Actividad8/act.js
+++ Actividad8/act.js
@@ -1,5 +1,7 @@
 var cartaB=0;
 var cartaJ=0;
+var puntuacion=0;
+
 function randCartaB(min,max) {
   cartaB=Math.round(Math.random()*(max - min) + min);
     return cartaB
@@ -10,42 +12,52 @@ function randCartaJ(min,max) {
     return cartaJ
 }
 
-randCartaJ(1,12);
-randCartaB(1,12);
-
-var msn=document.getElementById('cartaJugador');
-msn.innerHTML="Tu carta es el numero "+cartaJ;
 
 
 
 
-var pregunta=prompt("Tu carta es mayor o menor que la de la banca?");
+while(pregunta !="salir"){
+  randCartaJ(1,12);
+  randCartaB(1,12);
+  var pregunta=prompt("Tu carta es mayor o menor que la de la banca?");
+    if (pregunta=="mayor" || pregunta=="menor"){
+      comprobar();
+      var msn=document.getElementById('cartaJugador');
+      msn.innerHTML="Tu carta es el numero "+cartaJ;
 
-if (pregunta=="mayor" || pregunta=="menor"){
-  var msn=document.getElementById('cartaBanca');
-  msn.innerHTML="La carta que habia que adivinar es "+cartaB;
+      var msn=document.getElementById('cartaBanca');
+      msn.innerHTML="La carta que habia que adivinar es "+cartaB;
 
-  comprobar();
-}else{
-  var msn=document.getElementById('cartaJugador');
-  msn.innerHTML='Tienes que escribir "menor" o "mayor"';
-}
+    }
+  }
+
+
+
 
 function comprobar(){
     if (cartaJ<cartaB){
       if(pregunta=="menor"){
+        puntuacion=puntuacion+1;
         var msn=document.getElementById('puntuacion');
-        msn.innerHTML="Acertaste :D";
+        msn.innerHTML="Tu puntuacion es de " + puntuacion;
       }else{
+        puntuacion=puntuacion-1;
         var msn=document.getElementById('puntuacion');
-        msn.innerHTML="Fallaste :(";
+        msn.innerHTML="Tu puntuacion es de "+puntuacion;
       }
     }
       if(pregunta=="mayor"){
+        puntuacion=puntuacion+1;
         var msn=document.getElementById('puntuacion');
-        msn.innerHTML="Acertaste :D";
+        msn.innerHTML="Tu puntuacion es de "+puntuacion;
       }else{
+        puntuacion=puntuacion-1;
         var msn=document.getElementById('puntuacion');
-        msn.innerHTML="Fallaste :(";
+        msn.innerHTML="Tu puntuacion es de "+puntuacion;
     }
+    console.log("Tu carta "+randCartaJ(1,12));
+    console.log("La que se adivina "+randCartaB(1,12));
+    console.log(puntuacion);
+
+
   }
