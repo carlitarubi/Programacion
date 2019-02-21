cc4be615bc5e00de01828756ac7a899839c26bd9
LAPTOP-6J5DAT7D\Carlita <cacaru@floridauniversitaria.es>
2019-02-21 11:32:12 +0100

    proyecto7

---
+++ 7/listadoUsuarios.php
@@ -0,0 +1,54 @@
+<?php
+$conexion=new mysqli("localhost", "root", "", "juego");
+if ($conexion->connect_errno) {
+  echo "fallo al conectar MySql: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+}else {
+  $id=$_POST["id"];
+  $nombre=$_POST["nombre"];
+  $apellidos=$_POST["apellidos"];
+  $edad=$_POST["edad"];
+  $curso=$_POST["curso"];
+  $puntos=$_POST["puntos"];
+  $consulta="INSERT INTO usuario (id,nombre,apellidos,edad,curso,puntuacion) VALUES ('".$id."', '".$nombre."', '".$apellidos."', '".$edad."', '".$curso."','".$puntos."')";
+
+  $resultado = $conexion->query($consulta);
+  if($resultado==false){
+    echo $conexion->error;
+  }else{
+  echo "Inserción Correcta";
+  }
+    $resultado = $conexion->query("SELECT id,nombre,apellidos,edad,curso,puntuacion FROM usuario");
+}
+ ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table align='center'>
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Edad</th>
+        <th>Curso</th>
+        <th>Puntuación</th>
+     </tr>
+       <?php
+       foreach ($resultado as $usuario){
+        echo "<tr>";
+        echo "<td><a href=../6/juego.php?usuario=".$usuario['id'].">" .$usuario['id']."</td>";
+        echo "<td>" .$usuario['nombre']."</td>";
+        echo "<td>" .$usuario['apellidos']."</td>";
+        echo "<td>" .$usuario['edad']."</td>";
+        echo "<td>" .$usuario['curso']."</td>";
+        echo "<td>" .$usuario['puntuacion']."</td>";
+
+        echo "</tr>";
+      }
+        ?>
+   </table>
+  </body>
+</html>
---
+++ 7/registro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="listadoUsuarios.php" method="post">
+      <label for="id">ID</label>
+      <input type="text" id="id" name="id" value="">
+      <br><br>
+      <label for="nombre">Nombre</label>
+      <input type="text" id="nombre" name="nombre" value="">
+      <br><br>
+      <label for="apellidos">Apellido</label>
+      <input type="text" id="apellidos" name="apellidos" value="">
+      <br><br>
+      <label for="edad">Edad</label>
+      <input type="text" id="edad" name="edad" value="">
+      <br><br>
+      <label for="curso">Curso</label>
+      <input type="text" id="curso" name="curso" value="">
+      <br><br>
+      <label for="puntos">Puntuación</label>
+      <input type="text" id="puntos" name="puntos" value="">
+      <br><br>
+      <input type="submit" id="enviar" value="Enviar">
+    </form>
+  </body>
+</html>
